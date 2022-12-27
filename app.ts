4129e6d2237a74e38eb2d58a6a449b68f8aad7ea
Stefan Etzkorn <stefan.etzkorn@outlook.de>
2022-12-27 17:12:17 +0100

    init

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
 import express, {Express} from "express";
+import cors from "cors";
 import fileSearchRoutes from "./routes/FileSearchRoutes";
 
 const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/filesearch", fileSearchRoutes);
 
